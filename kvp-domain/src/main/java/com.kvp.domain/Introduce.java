34592b9e3a9e48a7aae10809195cf4a0e6945c57
MINDOCK <parksy8189@gmail.com>
2021-08-03 23:24:46 +0900

    ✨ step1: kafka streams 사용해보기
    - 요구사항
      - "kvp-input" 토픽에서 객체를 받는다.
      - 받은 객체에 나이를 10살 더하고 이름은 마스킹 처리한다.
      - "kvp-output" 토픽에 저장한다.

--- kvp-domain/src/main/java/com.kvp.domain/Introduce.java
+++ kvp-domain/src/main/java/com.kvp.domain/Introduce.java
@@ -7,11 +7,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder(toBuilder = true)
 public class Introduce {
     private String name;
     private Long age;
 
     public void addAge() {
         age += 10;
+    }
+
+    public void maskingName() {
+        if (name.length() <= 2) {
+            return;
+        }
+
+        name = name.replace(name.substring(1, name.length() - 1), "*");
     }
 }
--- kvp-streams/src/main/java/com/kvp/streams/StreamsApplication.java
+++ kvp-streams/src/main/java/com/kvp/streams/StreamsApplication.java
@@ -1,5 +1,18 @@
 package com.kvp.streams;
 
+import com.kvp.domain.Introduce;
+import com.kvp.streams.serdes.IntroduceSerde;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Properties;
+
 //https://coding-start.tistory.com/138
 public class StreamsApplication {
     public static void main(String[] args) {
@@ -10,5 +23,29 @@ public class StreamsApplication {
          *   - 나이를 10살 더해주세요.
          *   - 이름의 마스킹을 진행해 주세요.
          */
+        Serde<String> stringSerde= Serdes.String();
+        IntroduceSerde introduceSerde = new IntroduceSerde();
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, Introduce> changeIntroduceKStream = streamsBuilder.stream("kvp-input", Consumed.with(stringSerde, introduceSerde))
+                .mapValues((introduce) -> {
+                    Introduce copiedIntroduce = introduce.toBuilder().build();
+                    copiedIntroduce.maskingName();
+                    copiedIntroduce.addAge();
+                    return copiedIntroduce;
+                });
+
+        changeIntroduceKStream.to("kvp-output", Produced.with(stringSerde, introduceSerde));
+
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), getProperties());
+        kafkaStreams.start();
+    }
+
+    private static Properties getProperties() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kvp_streams_id");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return props;
     }
 }
